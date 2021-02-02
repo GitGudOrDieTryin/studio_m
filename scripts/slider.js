c3f935758f5e61972d554b70713b6b5d08327445
GitGudOrDieTrying <pryk.mak@gmail.com>
2021-02-02 23:20:09 +0200

    Finished mobile version, slider is working!

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,16 +1,40 @@
 'use strict';
 
 let selectors = document.querySelectorAll('.selector');
-let sliderImage = document.querySelector('.big-image');
-let button = document.getElementById('test')
+let slider = document.querySelector('.slider');
+let button = document.getElementById('test');
 
 let selectionIndex = 0;
 let swipeStartX = 0;
 let swipeEndX = 0;
 
+let bigImage1 = document.createElement("img");
+bigImage1.src = '../images/featuredListing1-1.jpg';
+bigImage1.classList.add('sliderBigImage');
+document.getElementById("bigImage").appendChild(bigImage1);
+
+let bigImage2 = document.createElement("img");
+bigImage2.src = '../images/featuredListing1-5.jpg';
+bigImage2.classList.add('sliderBigImage');
+document.getElementById("bigImage").appendChild(bigImage2);
+
+let bigImage3 = document.createElement("img");
+bigImage3.src = '../images/featuredListing1-9.jpg';
+bigImage3.classList.add('sliderBigImage');
+document.getElementById("bigImage").appendChild(bigImage3);
+
 eventsInicialization();
 
-button.addEventListener('click', () => console.log( selectionIndex ) )
+
+button.addEventListener('click', () => {
+    console.log( selectionIndex ) 
+    console.log( swipeStartX ) 
+    console.log( swipeEndX ) 
+
+});
+window.addEventListener('resize', e => console.log(e.target.innerWidth));
+
+
 
 function eventsInicialization() {
 
@@ -21,12 +45,12 @@ function eventsInicialization() {
         });
     }
     
-    sliderImage.addEventListener('touchstart', (event) => {
+    slider.addEventListener('touchstart', (event) => {
         event.preventDefault();
         swipeStartX = event.changedTouches[0].clientX; 
     });
     
-    sliderImage.addEventListener('touchend', (event) => {
+    slider.addEventListener('touchend', (event) => {
         event.preventDefault();
         swipeEndX = event.changedTouches[0].clientX;
         swipe();
@@ -38,66 +62,30 @@ function eventsInicialization() {
 function swipe(){
     if(swipeStartX - swipeEndX > 0 && selectionIndex < 2){
         selectionIndex++;
-        return 'right';
     } else if (swipeStartX - swipeEndX < 0 && selectionIndex > 0) {
         selectionIndex--;
     }
 }
 
-
 function slideChange() {
     switch(selectionIndex) {
         case 0:
-            sliderImage.classList.add('selector-1');
-            sliderImage.classList.remove('selector-2');
-            sliderImage.classList.remove('selector-3');
+            slider.classList.remove('position-2');
+            slider.classList.remove('position-3');
+            slider.classList.add('position-1');
         break;
+
         case 1:
-            sliderImage.classList.remove('selector-1');
-            sliderImage.classList.add('selector-2');
-            sliderImage.classList.remove('selector-3');
+            slider.classList.remove('position-1');
+            slider.classList.remove('position-3');
+            slider.classList.add('position-2');
         break;
+
         case 2:
-            sliderImage.classList.remove('selector-1');
-            sliderImage.classList.remove('selector-2');
-            sliderImage.classList.add('selector-3');
+            slider.classList.remove('position-1');
+            slider.classList.remove('position-2');
+            slider.classList.add('position-3');
         break;
 
     }
 }
-
-/*
-
-function selectionChange (index) {
-    if (index === 1){
-        sliderImage.classList.add('selector-1')
-        sliderImage.classList.remove('selector-2')
-        sliderImage.classList.remove('selector-3')
-    } else if (index === 2){
-        sliderImage.classList.remove('selector-1')
-        sliderImage.classList.add('selector-2')
-        sliderImage.classList.remove('selector-3')
-    } else {
-        sliderImage.classList.remove('selector-1')
-        sliderImage.classList.remove('selector-2')
-        sliderImage.classList.add('selector-3')
-    }
-}
-
-
-
-sliderImage.addEventListener('touchstart', (event) => {
-    let kek = event.changedTouches[0].clientX;
-    console.log( kek );
-});
-
-sliderImage.addEventListener('touchend', (event) => {
-    event.preventDefault();
-    let kek = event.changedTouches[0].clientX;
-    console.log( kek );
-});
-*/
-
-// функция считывает ивенты 
-// Функция которая расчитывает индекс
-// функция которая меняет картинку в ззависимости от индекса 
