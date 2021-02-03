397b677dba39f6624ed9afd6f1699ce10dc71c56
GitGudOrDieTrying <pryk.mak@gmail.com>
2021-02-03 02:49:02 +0200

    everything is broken now

--- scripts/slider.js
+++ scripts/slider.js
@@ -2,39 +2,13 @@
 
 let selectors = document.querySelectorAll('.selector');
 let slider = document.querySelector('.slider');
-let button = document.getElementById('test');
 
 let selectionIndex = 0;
 let swipeStartX = 0;
 let swipeEndX = 0;
 
-let bigImage1 = document.createElement("img");
-bigImage1.src = '../images/featuredListing1-1.jpg';
-bigImage1.classList.add('sliderBigImage');
-document.getElementById("bigImage").appendChild(bigImage1);
-
-let bigImage2 = document.createElement("img");
-bigImage2.src = '../images/featuredListing1-5.jpg';
-bigImage2.classList.add('sliderBigImage');
-document.getElementById("bigImage").appendChild(bigImage2);
-
-let bigImage3 = document.createElement("img");
-bigImage3.src = '../images/featuredListing1-9.jpg';
-bigImage3.classList.add('sliderBigImage');
-document.getElementById("bigImage").appendChild(bigImage3);
-
+generateImages();
 eventsInicialization();
-
-
-button.addEventListener('click', () => {
-    console.log( selectionIndex ) 
-    console.log( swipeStartX ) 
-    console.log( swipeEndX ) 
-
-});
-window.addEventListener('resize', e => console.log(e.target.innerWidth));
-
-
 
 function eventsInicialization() {
 
@@ -89,3 +63,95 @@ function slideChange() {
 
     }
 }
+
+function generateImages(){
+    let bigImage1 = document.createElement("img");
+    bigImage1.src = '../images/featuredListing1-1.jpg';
+    bigImage1.classList.add('sliderBigImage');
+    document.getElementById("bigImage").appendChild(bigImage1);
+
+    let smallImagesHolder1 = document.createElement("div");
+    smallImagesHolder1.classList.add('smallImagesHolder1');
+    document.getElementById("smallImage").appendChild(smallImagesHolder1);
+
+    let smallImage1 = document.createElement("img");
+    smallImage1.src = '../images/featuredListing1-2.jpg';
+    smallImage1.classList.add('sliderSmallImage');
+    document.querySelector(".smallImagesHolder1").appendChild(smallImage1);
+
+    let smallImage2 = document.createElement("img");
+    smallImage2.src = '../images/featuredListing1-3.jpg';
+    smallImage2.classList.add('sliderSmallImage');
+    document.querySelector(".smallImagesHolder1").appendChild(smallImage2);
+
+    let smallImage3 = document.createElement("img");
+    smallImage3.src = '../images/featuredListing1-4.jpg';
+    smallImage3.classList.add('sliderSmallImage');
+    document.querySelector(".smallImagesHolder1").appendChild(smallImage3);
+
+    //
+
+    let bigImage2 = document.createElement("img");
+    bigImage2.src = '../images/featuredListing1-5.jpg';
+    bigImage2.classList.add('sliderBigImage');
+    document.getElementById("bigImage").appendChild(bigImage2);
+
+    let smallImagesHolder2 = document.createElement("div");
+    smallImagesHolder2.classList.add('smallImagesHolder2');
+    document.getElementById("smallImage").appendChild(smallImagesHolder2);
+
+    let smallImage4 = document.createElement("img");
+    smallImage4.src = '../images/featuredListing1-6.jpg';
+    smallImage4.classList.add('sliderSmallImage');
+    document.querySelector(".smallImagesHolder2").appendChild(smallImage4);
+
+    let smallImage5 = document.createElement("img");
+    smallImage5.src = '../images/featuredListing1-7.jpg';
+    smallImage5.classList.add('sliderSmallImage');
+    document.querySelector(".smallImagesHolder2").appendChild(smallImage5);
+
+    let smallImage6 = document.createElement("img");
+    smallImage6.src = '../images/featuredListing1-8.jpg';
+    smallImage6.classList.add('sliderSmallImage');
+    document.querySelector(".smallImagesHolder2").appendChild(smallImage6);
+    
+    //
+
+    let bigImage3 = document.createElement("img");
+    bigImage3.src = '../images/featuredListing1-9.jpg';
+    bigImage3.classList.add('sliderBigImage');
+    document.getElementById("bigImage").appendChild(bigImage3);
+
+    let smallImagesHolder3 = document.createElement("div");
+    smallImagesHolder3.classList.add('smallImagesHolder3');
+    document.getElementById("smallImage").appendChild(smallImagesHolder3);
+
+    let smallImage7 = document.createElement("img");
+    smallImage7.src = '../images/featuredListing1-10.jpg';
+    smallImage7.classList.add('sliderSmallImage');
+    document.querySelector(".smallImagesHolder3").appendChild(smallImage7);
+
+    let smallImage8 = document.createElement("img");
+    smallImage8.src = '../images/featuredListing1-11.jpg';
+    smallImage8.classList.add('sliderSmallImage');
+    document.querySelector(".smallImagesHolder3").appendChild(smallImage8);
+
+    let smallImage9 = document.createElement("img");
+    smallImage9.src = '../images/featuredListing1-12.jpg';
+    smallImage9.classList.add('sliderSmallImage');
+    document.querySelector(".smallImagesHolder3").appendChild(smallImage9);
+}
+
+//All of this crap below was used for testing
+
+//let button = document.getElementById('test');
+
+//window.addEventListener('resize', e => console.log(e.target.innerWidth));
+
+/*
+button.addEventListener('click', () => {
+    console.log( selectionIndex ) 
+    console.log( swipeStartX ) 
+    console.log( swipeEndX ) 
+
+});*/
